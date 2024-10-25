638560e15a872975c6c7b246741cf4ad9eace826
An Nguen <ghostly.man256g@gmail.com>
2024-10-25 15:39:05 +0400

    Add a test for config file

--- OrderFilter.Tests/MainCommandTest.cs
+++ OrderFilter.Tests/MainCommandTest.cs
@@ -1,10 +1,55 @@
+using System.Reflection;
+using System.Text.Json;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Xunit.Abstractions;
 
 namespace OrderFilter.Tests;
 
-public class CliTest
+public class MainCommandTest
 {
+    private readonly ITestOutputHelper output;
+
+    public MainCommandTest(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public void Should_PrintOrdersToFile_On_ConfigFile()
+    {
+        // arrange
+        var app = new CommandApp<MainCommand>();
+        var dataFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "data.json");
+        var configFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "my-config.json");
+        var resultFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "my-orders.json");
+        var appSettings = new MainCommand.Settings()
+        {
+            InputFilePath = dataFilePath,
+            CityDistrict = "Ульяновск, Засвияжский район",
+            FirstDeliveryDateTime = DateTimeOffset.Parse("2024-01-16T10:30:00"),
+            DeliveryLogFilePath = "log0.txt",
+            DeliveryOrderFilePath = resultFilePath,
+        };
+        using (var fileStream = new FileStream(configFilePath, FileMode.Create, FileAccess.Write))
+        {
+            JsonSerializer.Serialize(fileStream, appSettings, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+        }
+        AnsiConsole.Record();
+
+        // act
+        var result = app.Run(["--config", configFilePath]);
+
+        // assert
+        output.WriteLine(AnsiConsole.ExportText());
+        output.WriteLine(dataFilePath);
+        Assert.Equal(0, result);
+        var text = File.ReadAllText(resultFilePath);
+        Assert.NotEmpty(text);
+
+        File.Delete(resultFilePath);
+    }
+
     [Fact]
     public void Should_Fail_On_InvalidInputFile()
     {
@@ -119,21 +164,21 @@ public class CliTest
     {
         // arrange
         var app = new CommandApp<MainCommand>();
-        Console.WriteLine(Directory.GetCurrentDirectory());
-        var id = new Guid();
-        string resultsFilePath = $"orders-{id}.json";
+        var id = Guid.NewGuid();
+        string resultsFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $"orders-{id}.json");
 
         // act
         var result = app.Run([
             "--input", "data.json",
             "--city-district", "Ульяновск, Засвияжский район",
-            "--first-delivery-date-time", "2024-01-15 11:40:00",
+            "--first-delivery-date-time", "2024-01-15T11:40:00",
+            "--delivery-log", $"{nameof(Should_PrintOrdersToFile)}.txt",
             "--delivery-order", resultsFilePath
         ]);
 
         // assert
-        var text = File.ReadAllText(resultsFilePath);
         Assert.Equal(0, result);
+        var text = File.ReadAllText(resultsFilePath);
         Assert.NotEmpty(text);
 
         File.Delete(resultsFilePath);
